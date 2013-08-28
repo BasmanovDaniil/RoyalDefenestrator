99c0d3d9dde90d1ce133807e6591f90fedbd8b01
Daniil Basmanov <basmanovdaniil@gmail.com>
2013-08-28 13:50:33 +0400

    Splitted vortex into vortex and shredder
    Hopefully removed some false bad endings

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    public Queen queen;
+    public Storyteller storyteller;
+
+    IEnumerator DestroyOther(GameObject other)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(other);
+    }
+
+    IEnumerator BadEnding()
+    {
+        yield return new WaitForSeconds(1);
+        storyteller.BadEnding();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(DestroyOther(other.gameObject));
+
+        if (other.tag == "Victim")
+        {
+            queen.KillVictim(other.transform);
+            if (other.name == "Cat")
+            {
+                queen.catKilled = true;
+                storyteller.catKilled = true;
+            }
+        }
+        if (other.tag == "Queen")
+        {
+            StartCoroutine(storyteller.GoodEndingAlt());
+        }
+
+        if (other.tag == "Cat" || other.name == "Cat")
+        {
+            queen.catKilled = true;
+            storyteller.catKilled = true;
+        }
+        if (other.tag == "Guard")
+        {
+            storyteller.guardCount--;
+        }
+        if (other.tag == "Page")
+        {
+            StartCoroutine(BadEnding());
+        }
+        if (other.tag == "FirstVictim")
+        {
+            storyteller.firstVictimDead = true;
+        }
+        if (other.tag == "AdmireVictim")
+        {
+            queen.KillAdmire();
+        }
+    }
+}
--- Assets/Scripts/Vortex.cs
+++ Assets/Scripts/Vortex.cs
@@ -1,12 +1,8 @@
-using System.Collections;
 using UnityEngine;
 
 public class Vortex : MonoBehaviour
 {
-    public float force = 1000;
-    public Queen queen;
-    public Storyteller storyteller;
-    public Guard[] guardList;
+    public float force = 3000;
 
     private Transform tr;
 
@@ -14,64 +10,6 @@ public class Vortex : MonoBehaviour
 	{
 	    tr = transform;
 	}
-
-    IEnumerator DestroyOther(GameObject other)
-    {
-        yield return new WaitForSeconds(3);
-        Destroy(other);
-    }
-
-    IEnumerator Play(AudioSource source)
-    {
-        yield return new WaitForSeconds(1);
-        source.Play();
-    }
-
-    IEnumerator BadEnding()
-    {
-        yield return new WaitForSeconds(1);
-        storyteller.BadEnding();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        StartCoroutine(DestroyOther(other.gameObject));
-        if (other.tag == "Victim")
-        {
-            queen.KillVictim(other.transform);
-            if (other.name == "Cat")
-            {
-                queen.catKilled = true;
-                storyteller.catKilled = true;
-            }
-        }
-        if (other.tag == "Queen")
-        {
-            StartCoroutine(storyteller.GoodEndingAlt());
-        }
-
-        if (other.tag == "Cat" || other.name == "Cat")
-        {
-            queen.catKilled = true;
-            storyteller.catKilled = true;
-        }
-        if (other.tag == "Guard")
-        {
-            storyteller.guardCount--;
-        }
-        if (other.tag == "Page")
-        {
-            StartCoroutine(BadEnding());
-        }
-        if (other.tag == "FirstVictim")
-        {
-            storyteller.firstVictimDead = true;
-        }
-        if (other.tag == "AdmireVictim")
-        {
-            queen.KillAdmire();
-        }
-    }
 
     void OnTriggerStay(Collider other)
     {
